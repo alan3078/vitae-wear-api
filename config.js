74d8be383c761089ab52d282b48e9ad1a2f19300
Wear Vitae <vitaewear.website@gmail.com>
2021-07-08 23:55:58 +0800

    update fileUpload

--- config.js
+++ config.js
@@ -14,7 +14,17 @@ const {
     PROJECT_ID,
     STORAGE_BUCKET,
     MESSAGING_SENDER_ID,
-    APP_ID
+    APP_ID,
+    FIREBASE_TYPE,
+    FIREBASE_PROJECT_ID,
+    FIREBASE_PRIVATE_KEY_ID,
+    FIREBASE_PRIVATE_KEY,
+    FIREBASE_CLIENT_EMAIL,
+    FIREBASE_CLIENT_ID,
+    FIREBASE_AUTH_URI,
+    FIREBASE_TOKEN_URI,
+    FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    FIREBASE_CLIENT_X509_CERT_URL
 } = process.env;
 
 assert(PORT, 'PORT is require');
@@ -24,7 +34,7 @@ module.exports = {
     port: PORT,
     host: HOST,
     url: HOST_URL,
-    firebaseConfig : {
+    firebaseConfig: {
         apiKey: API_KEY,
         authDomain: AUTH_DOMAIN,
         databaseURL: DATABASE_URL,
@@ -32,5 +42,18 @@ module.exports = {
         storageBucket: STORAGE_BUCKET,
         messagingSenderId: MESSAGING_SENDER_ID,
         appId: APP_ID
+    },
+    firebaseServiceConfig: {
+        "type": FIREBASE_TYPE,
+        "project_id": FIREBASE_PROJECT_ID,
+        "private_key_id": FIREBASE_PRIVATE_KEY_ID,
+        "private_key": FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        "client_email": FIREBASE_CLIENT_EMAIL,
+        "client_id": FIREBASE_CLIENT_ID,
+        "auth_uri": FIREBASE_AUTH_URI,
+        "token_uri": FIREBASE_TOKEN_URI,
+        "auth_provider_x509_cert_url": FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+        "client_x509_cert_url": FIREBASE_CLIENT_X509_CERT_URL
     }
+
 }
