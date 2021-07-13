7da12c67feb4eb23772b6c8c0fcd670880e51d00
Wear Vitae <vitaewear.website@gmail.com>
2021-07-14 00:00:32 +0800

    changing port

--- app.ts
+++ app.ts
@@ -10,7 +10,7 @@ import debug from 'debug';
 import path from 'path';
 const app: express.Application = express();
 const server: http.Server = http.createServer(app);
-const port = 3000;
+const port = process.env.PORT || 3000;
 const routes: Array<CommonRoutesConfig> = [];
 const debugLog: debug.IDebugger = debug('app');
 
